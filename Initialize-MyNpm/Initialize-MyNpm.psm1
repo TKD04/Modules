7136bcc22cd29d7a0dff3c96d91630c86567d5fa
TKD04 <tkd04devpub@outlook.com>
2023-06-20 02:52:38 +0900

    Add feature to remove `test` in npm scripts

--- Initialize-MyNpm/Initialize-MyNpm.psm1
+++ Initialize-MyNpm/Initialize-MyNpm.psm1
@@ -8,12 +8,16 @@ function Initialize-MyNpm {
     [OutputType([void])]
     param()
     process {
+        [string]$packagePath = '.\package.json'
         npm init -y
+        [hashtable]$package = $packagePath | Import-MyJSON -AsHashTable
+        $package.scripts.Remove('test')
+        $package | Export-MyJSON -LiteralPath $packagePath
         Add-Content -LiteralPath '.\.gitignore' -Value @(
             '/node_modules/'
             '/dist/'
         )
-        git add '.\.gitignore' '.\package.json'
+        git add '.\.gitignore' $packagePath
         git commit -m 'Add npm'
     }
 }
