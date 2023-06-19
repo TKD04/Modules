d0eedd4110502e38fce72efbbbf2f4729b0ee460
TKD04 <tkd04devpub@outlook.com>
2023-06-20 01:04:54 +0900

    Add Install-MyGulp

---
+++ Install-MyGulp/Install-MyGulp.psm1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+Adds gulp and its settings to the current directory.
+
+.PARAMETER UseTypeScript
+Whether to write gulpfile using TypeScript.
+
+#>
+function Install-MyGulp {
+    param (
+        [switch]$UseTypeScript
+    )
+    process {
+        [string]$gulpFilePath = '.\gulpfile.js'
+
+        if ($UseTypeScript) {
+            $gulpFilePath = $gulpFilePath -replace '.js$', '.ts'
+            npm i @types/gulp
+        }
+
+        npm i -D gulp gulp-cli
+        New-Item -Path '.\' -Name $gulpFilePath -ItemType 'File'
+
+        git add '.\package-lock.json' '.\package.json' $gulpFilePath
+    }
+}
+
+Export-ModuleMember -Function Install-MyGulp
