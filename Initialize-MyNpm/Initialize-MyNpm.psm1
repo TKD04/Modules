10ac39450997db8e0114327d67a516baa11e3b97
TKD04 <tkd04devpub@outlook.com>
2023-06-22 03:04:02 +0900

    Remove $packagePath

--- Initialize-MyNpm/Initialize-MyNpm.psm1
+++ Initialize-MyNpm/Initialize-MyNpm.psm1
@@ -8,16 +8,15 @@ function Initialize-MyNpm {
     [OutputType([void])]
     param()
     process {
-        [string]$packagePath = '.\package.json'
         npm init -y
-        [hashtable]$package = $packagePath | Import-MyJSON -AsHashTable
+        [hashtable]$package = '.\package.json' | Import-MyJSON -AsHashTable
         $package.scripts.Remove('test')
-        $package | Export-MyJSON -LiteralPath $packagePath
+        $package | Export-MyJSON -LiteralPath '.\package.json'
         Add-Content -LiteralPath '.\.gitignore' -Value @(
             '/node_modules/'
             '/dist/'
         )
-        git add '.\.gitignore' $packagePath
+        git add '.\.gitignore' '.\package.json'
         git commit -m 'Add npm'
     }
 }
