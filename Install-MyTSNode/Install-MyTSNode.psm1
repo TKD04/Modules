e5beed44bb6912106cc3ef1f52ff153a3c017130
TKD04 <tkd04devpub@outlook.com>
2023-06-23 18:43:26 +0900

    Add Install-MyTSNode

---
+++ Install-MyTSNode/Install-MyTSNode.psm1
@@ -0,0 +1,26 @@
+﻿<#
+.SYNOPSIS
+Adds ts-node to the current directory.
+#>
+function Install-MyTSNode {
+    [CmdletBinding()]
+    [OutputType([void])]
+    param ()
+    process {
+        [hashtable]$tsConfigTsnode = [ordered]@{
+            esm     = $true
+            require = @('ts-node/register')
+            swc     = $true
+        }
+
+        [hashtable]$tsConfig = Import-MyJSON -LiteralPath '.\tsconfig.json' -AsHashTable
+        $tsConfig.Add('ts-node', $tsConfigTsnode)
+        Export-MyJSON -LiteralPath '.\tsconfig.json' -CustomObject $tsConfig
+        npm i -D ts-node @swc/core @swc/helpers
+
+        git add '.\package-lock.json' '.\package.json' '.\tsconfig.json'
+        git commit -m 'Add ts-node'
+    }
+}
+
+Export-ModuleMember -Function 'Install-MyTSNode'
