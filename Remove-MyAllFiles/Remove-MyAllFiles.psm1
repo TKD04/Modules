927c05c2097580ed95ba2ff742d6dbd32467fa1d
TKD04 <tkd04devpub@outlook.com>
2023-06-24 01:02:39 +0900

    Add `Remove-MyAllFiles` as `rmallfiles`

---
+++ Remove-MyAllFiles/Remove-MyAllFiles.psm1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+Remove all files, not directories, from the current directory.
+#>
+function Remove-MyAllFiles {
+    [CmdletBinding()]
+    [Alias('rmallfiles')]
+    [OutputType([void])]
+    param ()
+    process {
+        [System.IO.FileInfo[]]$allFiles = Get-ChildItem -File
+        Remove-Item $allFiles -Confirm
+    }
+}
+
+Export-ModuleMember -Function Remove-MyAllFiles -Alias 'rmallfiles'
