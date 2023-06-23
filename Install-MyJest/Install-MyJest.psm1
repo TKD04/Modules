100a72a90b9965bf256bde86953f3fb2487bf9dd
TKD04 <tkd04devpub@outlook.com>
2023-06-24 04:00:04 +0900

    Add specific configs and `$UseTypeScript` to param

--- Install-MyJest/Install-MyJest.psm1
+++ Install-MyJest/Install-MyJest.psm1
@@ -1,19 +1,67 @@
 <#
 .SYNOPSIS
 Adds Jest and its settings to the current directory.
+
+.PARAMETER UseTypeScript
+Whether to support TypeScript.
+
+.PARAMETER UseBrowser
+Whether to support browser (dom).
+
+.PARAMETER UseNode
+Whether to support Node.
 #>
 function Install-MyJest {
     [CmdletBinding()]
     [OutputType([void])]
-    param()
+    param(
+        [switch]$UseTypeScript,
+        [switch]$UseBrowser,
+        [switch]$UseNode
+    )
     process {
+        if (!$UseBrowser -and !$UseNode) {
+            throw 'Either $UseBrowser or $UseNode must be enabled.'
+        }
+        if ($UseBrowser -and $UseNode) {
+            throw 'Only either $UseBrowser or $UseNode can be enabled.'
+        }
+
+        [string]$jestConfigPath = '.\jest.config.js'
+        [string[]]$neededPackages = @(
+            'jest'
+        )
+
+        if ($UseTypeScript) {
+            $neededPackages += @(
+                'ts-jest'
+                '@types/jest'
+            )
+            if ($UseBrowser) {
+                [string]$SrcJestConfigPath = Join-Path -Path $PSScriptRoot -ChildPath '.\ts.browser.jest.config.js'
+                Copy-Item -LiteralPath $SrcJestConfigPath -Destination $jestConfigPath
+            }
+            if ($UseNode) {
+                [string]$SrcJestConfigPath = Join-Path -Path $PSScriptRoot -ChildPath '.\ts.node.jest.config.js'
+                Copy-Item -LiteralPath $SrcJestConfigPath -Destination $jestConfigPath
+            }
+        }
+        else {
+            if ($UseBrowser) {
+                [string]$SrcJestConfigPath = Join-Path -Path $PSScriptRoot -ChildPath '.\browser.jest.config.js'
+                Copy-Item -LiteralPath $SrcJestConfigPath -Destination $jestConfigPath
+            }
+            if ($UseNode) {
+                [string]$SrcJestConfigPath = Join-Path -Path $PSScriptRoot -ChildPath '.\node.jest.config.js'
+                Copy-Item -LiteralPath $SrcJestConfigPath -Destination $jestConfigPath
+            }
+        }
         Add-MyNpmScript -NameToScript @{
             'test' = 'jest'
         }
-        npm i -D jest ts-jest @types/jest
-        npx ts-jest config:init
+        npm i -D $neededPackages
 
-        git add '.\jest.config.js' '.\package-lock.json' '.\package.json'
+        git add '.\package-lock.json' '.\package.json' $jestConfigPath
         git commit -m 'Add Jest'
     }
 }
