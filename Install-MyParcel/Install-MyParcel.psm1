43a0780b9e3ad4533ed3776a55b346fc2bce915a
TKD04 <tkd04devpub@outlook.com>
2023-07-03 02:20:26 +0900

    Add Install-MyParcel

---
+++ Install-MyParcel/Install-MyParcel.psm1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+Adds parcel to the current directory.
+#>
+function Install-MyParcel {
+    [CmdletBinding()]
+    [OutputType([void])]
+    param ()
+    process {
+        Add-MyNpmScript -NameToScript @{
+            'dev'   = 'parcel build ./src/index.html'
+            'watch' = 'parcel serve ./src/index.html --open'
+        }
+        Add-Content -LiteralPath '.\.gitignore' -Value '/.parcel-cache/'
+        npm i -D parcel
+
+        git add '.\.gitignore' '.\package-lock.json' '.\package.json'
+        git commit -m 'Add parcel'
+    }
+}
+
+Export-ModuleMember -Function Install-MyParcel
