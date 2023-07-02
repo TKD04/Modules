d062f89c68497844c6eac477ed411584a1221ba7
TKD04 <tkd04devpub@outlook.com>
2023-07-02 20:50:05 +0900

    Add `$UseTypeScript` and `$UseJect` params

--- Install-MyEnvForNode/Install-MyEnvForNode.psm1
+++ Install-MyEnvForNode/Install-MyEnvForNode.psm1
@@ -3,28 +3,63 @@ Import-Module -Name 'New-UserVariables'
 <#
 .SYNOPSIS
 Adds the Node develop environment to the current directory.
+
+.PARAMETER UseTypeScript
+Whether to support TypeScript.
+
+.PARAMETER UseJest
+Whether to use Jest.
 #>
 function Install-MyEnvForNode {
     [CmdletBinding()]
     [Alias('ienvnode')]
     [OutputType([void])]
-    param()
+    param(
+        [switch]$UseTypeScript,
+        [switch]$UseJest
+    )
     process {
         Initialize-MyGit
         Initialize-MyNpm
-        Install-MyTypeScript -UseNode
-        Install-MyESLint -UseTypeScript -UseNode -UseJest
-        Install-MyJest -UseTypeScript -UseNode
+        if ($UseTypeScript -and $UseJest) {
+            Install-MyTypeScript -UseNode
+            Install-MyESLint -UseTypeScript -UseNode -UseJest
+            Install-MyJest -UseTypeScript -UseNode
+        }
+        elseif ($UseTypeScript -and !$UseJest) {
+            Install-MyTypeScript -UseNode
+            Install-MyESLint -UseTypeScript -UseNode
+        }
+        elseif (!$UseTypeScript -and $UseJest) {
+            Install-MyESLint -UseNode -UseJest
+            Install-MyJest -UseNode
+        }
+        else {
+            Install-MyESLint -UseNode
+        }
         Install-MyVSCodeSettingsForWeb
         New-Item -Path '.\' -Name 'src' -ItemType 'Directory'
-        Add-MyNpmScript -NameToScript @{
-            'watch' = 'nodemon --watch "src/**/*.ts" --exec "ts-node" src/index.ts'
+        if ($UseTypeScript) {
+            Add-MyNpmScript -NameToScript @{
+                'watch' = 'nodemon --watch "src/**/*.ts" --exec "ts-node" src/index.ts'
+            }
+            npm i -D ts-node nodemon
+
+            git add '.\package-lock.json'
+        }
+        else {
+            Add-MyNpmScript -NameToScript @{
+                'watch' = 'node --watch "./src"'
+            }
         }
         Join-Path -Path $gitignoreDirPath -ChildPath 'Node.gitignore' |
         Copy-Item -Destination '.\.gitignore'
-        npm i -D ts-node nodemon
+        <# Add "type": "module" to package.json #>
+        [hashtable]$package = Import-MyJSON -LiteralPath '.\package.json' -AsHashTable
+        $package.Add('type', 'module')
+        Export-MyJSON -LiteralPath '.\package.json' -CustomObject $package
 
-        git add '.\.eslintrc.json' '.\.gitignore' '.\package.json'
+        git add '.\.gitignore' '.\package.json'
         git commit -m 'Add environment for Node'
     }
 }
