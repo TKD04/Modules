9272eae7e3ae669568dc740dce7c76824cec5614
TKD04 <tkd04devpub@outlook.com>
2023-07-03 11:14:17 +0900

    Add Install-MyExpress

---
+++ Install-MyExpress/Install-MyExpress.psm1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+Adds Express to the current directory.
+
+.PARAMETER $UseTypeScript
+Whether to add type definitions of Express.
+#>
+function Install-MyExpress {
+    [CmdletBinding()]
+    [OutputType([void])]
+    param (
+        [switch]$UseTypeScript
+    )
+    process {
+        npm i -D express
+        if ($UseTypeScript) {
+            npm i -D @types/express
+        }
+
+        git add '.\package-lock.json' 'package.json'
+        git commit -m 'Add Express'
+    }
+}
+
+Export-ModuleMember -Function Install-MyExpress
