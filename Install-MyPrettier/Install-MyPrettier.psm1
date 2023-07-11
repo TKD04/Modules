5cc80c43ad740524ef96c7d66de2f67b6194bdfc
TKD04 <tkd04devpub@outlook.com>
2023-07-11 23:09:20 +0900

    Add Install-MyPrettier

---
+++ Install-MyPrettier/Install-MyPrettier.psm1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+Adds Prettier to the current directory.
+#>
+function Install-MyPrettier {
+    [CmdletBinding()]
+    [OutputType([void])]
+    param ()
+    process {
+        npm i -D prettier
+        Add-MyNpmScript -NameToScript @{
+            'format' = 'prettier . --write'
+        }
+
+        git add '.\package-lock.json' '.\package.json'
+        git commit -m 'Add Prettier'
+    }
+}
+
+Export-ModuleMember -Function Install-MyPrettier
