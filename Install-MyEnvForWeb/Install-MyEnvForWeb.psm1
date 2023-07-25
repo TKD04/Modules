c1140dea18924ce0b94e600e65d7aeb94b922b2c
TKD04 <tkd04devpub@outlook.com>
2023-07-25 20:43:38 +0900

    Add Install-MyEnvForWeb as ienvweb

---
+++ Install-MyEnvForWeb/Install-MyEnvForWeb.psm1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+Adds the web develop environment to the current directory.
+#>
+function Install-MyEnvForWeb {
+    [CmdletBinding()]
+    [Alias('ienvweb')]
+    [OutputType([void])]
+    param ()
+    process {
+        # TODO: Add UseTypeScript switch
+        # TODO: Add UseReact switch
+        Initialize-MyGit
+        Initialize-MyNpm
+        Install-MyTypeScript
+        Install-MyESLint -UseTypeScript -UseJest -UseBrower
+        Install-MyJest -UseTypeScript -UseBrowser
+        Install-MyPrettier
+        Install-MyWebpack
+    }
+}
+
+Export-ModuleMember -Function Install-MyEnvForWeb -Alias 'ienvweb'
