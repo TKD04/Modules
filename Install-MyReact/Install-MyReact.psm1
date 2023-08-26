4ea2dcf55bf32d513d47be0b508f454442a45c2a
TKD04 <tkd04devpub@outlook.com>
2023-08-26 20:53:03 +0900

    Add Install-MyReact

---
+++ Install-MyReact/Install-MyReact.psm1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+Adds React to the current directory.
+
+.PARAMETER UseTypeScript
+Whether to support TypeScript.
+#>
+function Install-MyReact {
+    [CmdletBinding()]
+    [OutputType([void])]
+    param (
+        [switch]$UseTypeScript
+    )
+    process {
+        npm i -D react react-dom
+        if ($UseTypeScript) {
+            npm i -D @types/react @types/react-dom
+        }
+
+        git add '.\package-lock.json' '.\package.json'
+        git commit -m 'Add React'
+    }
+}
+
+Export-ModuleMember -Function Install-MyReact
