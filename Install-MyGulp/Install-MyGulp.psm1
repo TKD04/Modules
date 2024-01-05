357436f77651f9fbbf58f8646e61a28ad68b45ab
TKD04 <tkd04devpub@outlook.com>
2024-01-05 13:51:13 +0900

    Add $UseTypeScript

--- Install-MyGulp/Install-MyGulp.psm1
+++ Install-MyGulp/Install-MyGulp.psm1
@@ -1,14 +1,23 @@
 <#
 .SYNOPSIS
 Adds gulp to the current directory.
+
+.PARAMETER UseTypeScript
+Whether to add the rules for TypeScript.
 #>
 function Install-MyGulp {
     [CmdletBinding()]
     [OutputType([void])]
-    param ()
+    param (
+        [switch]$UseTypeScript
+    )
     process {
         npm i -D gulp gulp-cli
         New-Item -Path '.\' -Name 'gulpfile.mjs' -ItemType 'File'
+
+        if ($UseTypeScript) {
+            npm i -D @types/gulp
+        }
 
         git add '.\package-lock.json' '.\package.json' '.\gulpfile.mjs'
         git commit -m 'Add gulp'
