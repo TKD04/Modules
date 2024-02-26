a39a8aac5f70f6d531b27e80f046ce1072bc9bbf
TKD04 <tkd04fordev@outlook.com>
2024-02-26 16:10:21 +0900

    Add feature to install `prettier-plugin-tailwindcss`

--- Install-MyPrettier/Install-MyPrettier.psm1
+++ Install-MyPrettier/Install-MyPrettier.psm1
@@ -1,16 +1,31 @@
 ﻿<#
 .SYNOPSIS
 Adds Prettier to the current directory.
+
+.PARAMETER UseTailwindcss
+Whether to support automatic class sorting of tailwindcss.
 #>
 function Install-MyPrettier {
     [CmdletBinding()]
     [OutputType([void])]
-    param ()
+    param (
+        [switch]$UseTailwindcss
+    )
     process {
-        npm i -D prettier
+        [string[]]$neededDevPackages = @(
+            'prettier'
+        )
+
+        if ($UseTailwindcss) {
+            [string]$srcPrettierConfigPath = Join-Path -Path $PSScriptRoot -ChildPath '.\prettier.config.js'
+            Copy-Item -LiteralPath $srcPrettierConfigPath -Destination '.\prettier.config.js'
+            $neededDevPackages += 'prettier-plugin-tailwindcss'
+            git add '.\prettier.config.js'
+        }
         Add-MyNpmScript -NameToScript @{
             'format' = 'prettier . --write'
         }
+        npm i -D $neededDevPackages
 
         git add '.\package-lock.json' '.\package.json'
         git commit -m 'Add Prettier'
