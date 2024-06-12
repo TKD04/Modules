700ac397a1e94aa993a561a6fb91b377d505bd4f
TKD04 <tkd04fordev@outlook.com>
2024-06-12 16:56:54 +0900

    Add `Install-MyTailwindCss`

---
+++ Install-MyTailwindCss/Install-MyTailwindCss.psm1
@@ -0,0 +1,42 @@
+﻿<#
+.SYNOPSIS
+Adds Tailwind CSS to the current directory.
+
+.PARAMETER UseDaisyUi
+Whether to use daisyUI.
+
+.PARAMETER IsVite
+Wether to support a project created by Vite.
+#>
+function Install-MyTailwindCss {
+    [CmdletBinding()]
+    [OutputType([void])]
+    param (
+        [switch]$UseDaisyUi,
+        [switch]$IsVite
+    )
+    process {
+        npm i -D tailwindcss
+        Copy-Item -LiteralPath "$PSScriptRoot\tailwind.config.js" -Destination '.\tailwind.config.js'
+        if ($UseDaisyUi) {
+            npm i -D daisyui@latest
+            Copy-Item -LiteralPath "$PSScriptRoot\daisyui-tailwind.config.js" -Destination '.\tailwind.config.js'
+        }
+        else {
+            Copy-Item -LiteralPath "$PSScriptRoot\tailwind.config.js" -Destination '.\tailwind.config.js'
+        }
+        if ($IsVite) {
+            Copy-Item -LiteralPath "$PSScriptRoot\vite-index.css" -Destination '.\src\index.css' -Force
+            git add '.\src\index.css'
+        }
+        else {
+            Copy-Item -LiteralPath "$PSScriptRoot\style.css" -Destination '.\style.css' -Force
+            git add '.\style.css'
+        }
+
+        git add '.\package-lock.json' '.\package.json' '.\tailwind.config.js'
+        git commit -m 'Add Tailwind CSS'
+    }
+}
+
+Export-ModuleMember -Function Install-MyTailwindCss
